9df22ab0aec2beab8ae6feece37889f21cec14ec
JGS-JAVA <wlrncom@naver.com>
2025-01-20 17:51:08 +0900

    Initial commit

---
+++ src/main/java/com/kht/ecommerce/ecommerce_application/controller/ViewController.java
@@ -0,0 +1,63 @@
+package com.kht.ecommerce.ecommerce_application.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class ViewController {
+
+    // 사용자 목록 페이지
+    @GetMapping("/")
+    public String usersPage() {
+        return "index";
+    }
+
+    // 상품 목록 페이지
+    @GetMapping("/products")
+    public String productsPage() {
+        return "products";
+    }
+
+    // 장바구니 페이지
+    @GetMapping("/cart")
+    public String cartPage() {
+        return "cart";
+    }
+
+    // 유저 장바구니 페이지
+    @GetMapping("/cart{userId}")
+    public String getCartByUserId(@PathVariable("userId") int id) {
+        return "cart";
+    }
+
+    // 회원가입 페이지 볼 수 있게 회원가입.html 불러오기
+    @GetMapping("/join")
+    public String joinPage() {
+        return "join";
+    }
+
+    @GetMapping("/products/insert")
+    public String getProduct() {
+        return "insertProduct";
+    }
+
+    @GetMapping("/user/{id}")
+    public String getUserById() {
+        return "user_detail";
+    }
+
+    @GetMapping("/product/{id}")
+    public String getProductById() {
+        return "product_detail";
+    }
+}
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/kht/ecommerce/ecommerce_application/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.kht.ecommerce.ecommerce_application.mapper;
+
+import com.kht.ecommerce.ecommerce_application.dto.User;
+import org.apache.ibatis.annotations.Mapper;
+import java.util.List;
+
+@Mapper
+public interface UserMapper {
+    // 사용자 조회
+    List<User>  getAllUsers();
+    // 사용자 저장
+    void insertUser(User user);
+
+    // 이메일 존재 유무 확인
+    int existByEmail(String email);
+}
