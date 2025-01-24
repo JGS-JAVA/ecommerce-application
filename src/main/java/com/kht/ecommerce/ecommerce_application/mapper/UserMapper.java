0c70b579652401a49b077f9e2303e8534c3cc07b
JGS-JAVA <wlrncom@naver.com>
2025-01-24 17:08:13 +0900

    commit

--- src/main/java/com/kht/ecommerce/ecommerce_application/mapper/UserMapper.java
+++ src/main/java/com/kht/ecommerce/ecommerce_application/mapper/UserMapper.java
@@ -6,9 +6,10 @@ import java.util.List;
 
 @Mapper
 public interface UserMapper {
+    // 디비에서 데이터 가져올 때 DTO 사용
     // 사용자 조회
     List<User>  getAllUsers();
-    // 사용자 저장 -> 다수저장시 void 를 int 로 바꿔 몇개 저장하는지 적는다
+    // 사용자 저장 -> 다수 저장시 갯수 중요하면 void 를 int 로 바꿔 몇개 저장하는지 적는다
     void insertUser(User user);
 
     // 이메일 존재 유무 확인
@@ -16,5 +17,6 @@ public interface UserMapper {
     User getUserById(int id);
     
     // put과 post 는 int get = 몇개 찾았는지 적는다
+    // 1개만 수정시 int 를 void 로 작성가능. 다수 수정시 int 작성
     int editUser(User user);
 }
